9192e2b96b85e67e9b7fb585848dd74a65ffd5c0
Eliveltonbyte <eliveltonwess@gmail.com>
2024-09-10 09:56:25 -0300

    Inquirer

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+const { select } = require('@inquirer/prompts')
+
+
+const start = async () => {
+
+    while (true) {
+
+        const opcao = await select({
+            message: "Menus >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+
+        switch (opcao) {
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "listar":
+                console.log("Vamos listar")
+                break
+            case "sair":
+                return
+        }
+    }
+}
+
+start()
